0bbbc5ebacd3a78f2cab85d900cb84b4974b40c0
Travis A. Carlen <tcarlen@ucsc.edu>
2023-04-08 14:26:09 -0700

    Added score + sfx

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,8 +3,38 @@ class Menu extends Phaser.Scene {
         super("menuScene");
     }
 
+    preload() {
+        this.load.audio('sfx_select', './assets/select.wav');
+        this.load.audio('sfx_explosion', './assets/delayed_blast.wav');
+        this.load.audio('sfx_rocket', './assets/rocket_shoot.wav');
+      }
+
     create(){
+
         this.add.text(20, 20, "Rocket Patrol Menu");
-        this.scene.start("playScene");
+
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+          // easy mode
+          game.settings = {
+            spaceshipSpeed: 3,
+            gameTimer: 60000    
+          }
+          this.sound.play('sfx_select');
+          this.scene.start('playScene');    
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+          // hard mode
+          game.settings = {
+            spaceshipSpeed: 4,
+            gameTimer: 45000    
+          }
+          this.sound.play('sfx_select');
+          this.scene.start('playScene');    
+        }
+      }
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -40,15 +40,48 @@ class Play extends Phaser.Scene {
             frames: this.anims.generateFrameNumbers('explosion',  {start: 0, end: 9, first: 0}),
             frameRate: 30
         });
+
+        this.p1Score = 0;
+
+        let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3b141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5
+            },
+            fixedWidth:100
+        }
+
+        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
+
+        this.gameOver = false;
+
+        scoreConfig.fixedWidth = 0;
+        this.clock = this.time.delayedCall(60000, () => {
+            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+            this.gameOver = true;
+        }, null, this);
     }
 
     update(){
         this.starfield.tilePositionX -=4;
 
-        this.p1Rocket.update();
-        this.ship01.update();
-        this.ship02.update();
-        this.ship03.update();
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+            this.sound.play('sfx_select');
+            this.scene.restart();
+        }
+
+        if (!this.gameOver){
+            this.p1Rocket.update();
+            this.ship01.update();
+            this.ship02.update();
+            this.ship03.update();
+        }
 
         for (let i of this.livingShips){
             if(this.checkCollision(this.p1Rocket, i)) {
@@ -79,5 +112,9 @@ class Play extends Phaser.Scene {
             ship.alpha = 1;
             boom.destroy();
         })
+
+        this.p1Score += ship.points;
+        this.scoreLeft.text = this.p1Score;
+        this.sound.play('sfx_explosion');
     }
 }
