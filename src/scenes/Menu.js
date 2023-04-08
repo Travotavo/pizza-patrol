5e84a7d31a0e0f2d46347c590279a09073db1029
Travis A. Carlen <tcarlen@ucsc.edu>
2023-04-08 14:34:37 -0700

    Finished up menu and difficulty, tutorial finished

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -11,8 +11,24 @@ class Menu extends Phaser.Scene {
 
     create(){
 
-        this.add.text(20, 20, "Rocket Patrol Menu");
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3b141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5
+            },
+            fixedWidth:0
+        }
 
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire!', menuConfig).setOrigin(0.5);
+        menuConfig.backgroundColor = '#00FF00';
+        menuConfig.color = '#000';
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Use ← for Novice and → for Expert', menuConfig).setOrigin(0.5);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
